026f6bf83f69a5172b930d12b331936cbc9bab4f
jlyajure <jlyt1710@gmail.com>
2026-02-11 09:48:53 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Importamos las herramientas de Google
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Tus datos secretos (NO los borres, usa los que ya tenías)
 const firebaseConfig = {
-  apiKey: "AIzaSyBwBq4gLgv4DSfUidzUuC7Irmvj_4pCTtI",
-  authDomain: "familia-yajure-app.firebaseapp.com",
-  projectId: "familia-yajure-app",
-  storageBucket: "familia-yajure-app.firebasestorage.app",
-  messagingSenderId: "692035727386",
-  appId: "1:692035727386:web:dfa3e39a481d56368a61a3",
-  measurementId: "G-PLRGRQT36L"
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_ID",
+  appId: "TU_APP_ID"
 };
 
-// Initialize Firebase
+// Encendemos Firebase y la Base de Datos
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+// Exportamos la base de datos para usarla en la App
+export { db };
