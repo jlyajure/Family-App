7d3c418d6be7b4ea6fabfb1ecd96dbc9a4ae84cd
jlyajure <jlyt1710@gmail.com>
2026-02-11 16:21:00 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,19 +2,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Tus datos secretos (NO los borres, usa los que ya tenías)
+// Tus datos reales de Firebase Yajure App
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBwBq4gLgv4DSfUidzUuC7Irmvj_4pCTtI",
+  authDomain: "familia-yajure-app.firebaseapp.com",
+  projectId: "familia-yajure-app",
+  storageBucket: "familia-yajure-app.firebasestorage.app",
+  messagingSenderId: "692035727386",
+  appId: "1:692035727386:web:dfa3e39a481d56368a61a3",
+  measurementId: "G-PLRGRQT36L"
 };
 
 // Encendemos Firebase y la Base de Datos
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Exportamos la base de datos para usarla en la App
+// Exportamos la base de datos para que index.html la pueda usar
 export { db };
